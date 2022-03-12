6059fa46c58472d17d748f28ab731f9abfe8ba83
honordevop <ogunladestephen20@gmail.com>
2022-03-12 02:01:45 -0800

    script modified

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  * Code block prints out a string
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
