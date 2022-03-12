8637d24275d753d91a18c41becbc76d2e354e224
honordevop <ogunladestephen20@gmail.com>
2022-03-12 00:53:03 -0800

    trailing spaces removed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h> 
-int main (void)
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
