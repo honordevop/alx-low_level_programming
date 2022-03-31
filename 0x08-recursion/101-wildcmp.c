7281492d83c8201166892eec9f455cd774f85555
patrick-hub <patrick4akuagwu@yahoo.com>
2022-03-31 23:58:56 +0100

    Bonu project modification

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,34 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * wildcmp - check the code for Holberton School students.
- * @s1: string
- * @s2: string
- * Return: Always 0.
+ * wildcmp - A function that compares two strings
+ * @s1: First input string
+ * @s2: Second input string
+ * Return: 1 if strings identical and 0 in otherwise
  */
-
 int wildcmp(char *s1, char *s2)
 {
 return (checker(s1, s2, 0, 0, -1));
 }
 
-/**
- * checkLast - check last char of s2 when s1 ends
- * @s: string
- * @i: int
- * Return: 0 or 1
- */
-int checkLast(char *s, int i)
-{
-if (s[i] == '*')
-	return (checkLast(s, i + 1));
-else if (s[i] == '\0')
-	return (1);
-
-return (0);
-
-}
 /**
  * checker - helper
  * @s1: string
@@ -41,32 +22,49 @@ return (0);
 int checker(char *s1, char *s2, int a, int b, int wildUsed)
 {
 
+//printf("s1 <%c> a <%i> s2 <%c> b <%i> \n", s1[a], a, s2[b], b);
+
 if (s1[a] != '\0')
-{
+{	
 	if (s2[b] == '\0')
 		return (0);
 	else if (s2[b] == '*')
 	{
-		if (s2[b + 1] == '*')
+		if (s2[b + 1] == '*'){
+			//printf("Pass 1\n");
 			return (checker(s1, s2, a, b + 1, b));
-		else if (s2[b + 1] == s1[a])
+		}
+		else if (s2[b + 1] == s1[a]){
+			//printf("Pass 2\n");
 			return (checker(s1, s2, a, b + 1, b));
-		else if (s1[a + 1] != s2[b + 1])
-			return (checker(s1, s2, a + 1, b, b));
-		else if (s1[a + 1] == s2[b + 1])
-			return (checker(s1, s2, a + 1, b + 1, b));
+		}	
+		else if (s1[a + 1] != s2[b + 1]){
+			//printf("Pass 3\n");
+			return (checker(s1, s2, a + 1, b, b));	
+		}
+		else if (s1[a + 1] == s2[b + 1]){
+			//printf("Pass 3\n");
+			return (checker(s1, s2, a + 1, b + 1, b));	
+		}
+		
 	}
-	else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
+	else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1])){
+		//printf("Pass 4\n");
 		return (checker(s1, s2, a + 1, b + 1, wildUsed));
-
-	if (wildUsed == -1)
-		return (0);
-
-	return (checker(s1, s2, a, wildUsed, wildUsed));
-
+	}
+	else
+	{
+		if (wildUsed == -1)
+		{
+			//printf("No wild card\n");
+			return (0);
+		}
+		//printf("Back to wildcard\n");
+		return (checker(s1, s2, a, wildUsed, wildUsed));
+	}
 }
 if (s2[b] != '\0')
-	return (checkLast(s2, b));
+	return 0;
 
 return (1);
 
